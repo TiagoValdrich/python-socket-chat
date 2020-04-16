706f3be1f09acc35061fcfb775f7fd08ca00a2c6
Tiago José Valdrich <35928505+TiagoValdrich@users.noreply.github.com>
2020-04-16 20:41:37 -0300

    Removing unnusable thread on client.py

--- client.py
+++ client.py
@@ -1,13 +1,4 @@
-import socket, threading
-
-def handle_server_messages(socket: socket.socket) -> None:
-    while True:
-        try:
-            server_msg = socket.recv(1024)
-            print(server_msg.decode())
-        except Exception as e:
-            print(f'Error handling server messages, closing conection: {e}')
-            socket.close()
+import socket
 
 def client() -> None:
     SERVER_ADDRESS = '127.0.0.1'
@@ -20,9 +11,6 @@ def client() -> None:
         socket_instance.connect((SERVER_ADDRESS, SERVER_PORT))
 
         while True:
-            if not open_thread:
-                open_thread = threading.Thread(target=handle_server_messages, args=(socket_instance))
-
             msg = input('Insert a message: ')
 
             if msg == 'quit':
